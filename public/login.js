e6f5364892d5db0378b0c964d7020539c35e44b8
erico89 <72563708+erico89@users.noreply.github.com>
2023-02-15 20:30:58 +0100

    LOGIN CLIENT SIDE 5%
    para pasarmelo a mi pc

---
+++ public/login.js
@@ -0,0 +1,13 @@
+
+var login {
+
+    init: function
+    {
+        //this.uinput = this.getSelector("#user_name");
+    },
+    //Function to get the selector (easier code)
+    getSelector: function(selector)
+    {
+        return document.querySelector(selector);
+    },
+}
