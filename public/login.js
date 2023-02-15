eb91f1fb36d968b9aede4cd7ac371fd137934cdf
erico89 <72563708+erico89@users.noreply.github.com>
2023-02-15 22:22:36 +0100

    login client not working yet

--- public/login.js
+++ public/login.js
@@ -1,10 +1,35 @@
 
 var login {
 
-    init: function
+    init: function()
     {
-        //this.uinput = this.getSelector("#user_name");
+        //Log in data
+        this.loginu = this.getSelector('input[name="lusername"]');
+        this.loginp = this.getSelector('input[name="lpassword"]');
+
+        //Sign up data
+        this.signupu = this.getSelector('input[name="susername"]');
+        this.signupp1 = this.getSelector('input[name="spassword1"]');
+        this.signupp2 = this.getSelector('input[name="spassword2"]');
+
+        //button
+        this.bttn = this.getSelector('#button');
+
+        //labels
+        this.toplbl = this.getSelector('.label');
+        this.botlbl = this.getSelector('.change');
     },
+
+    changeToSingUp: function()
+    {
+        alert(this.toplbl.value)
+    },
+
+    changeToLogIn: function()
+    {
+
+    },
+
     //Function to get the selector (easier code)
     getSelector: function(selector)
     {
