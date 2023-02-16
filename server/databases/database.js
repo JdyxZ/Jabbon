eade56610e13f1af0e22b764def1492c43ef10e5
Haylo <javier.serrano04@estudiant.upf.edu>
2023-02-16 11:01:52 +0100

    Module update

--- server/databases/database.js
+++ server/databases/database.js
@@ -1,1 +1,14 @@
 import { createPool } from "mysql2/promise";
+import {
+    DB_DATABASE,
+    DB_HOST,
+    DB_PASSWORD,
+    DB_USER,
+} from "./config.js";
+
+export const pool = await createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE
+})
