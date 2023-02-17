cb1c01dedc9909393a345fc8db9a0b818d5ae73f
Haylo <javier.serrano04@estudiant.upf.edu>
2023-02-17 15:17:26 +0100

    MySQL Database Config

--- server/database/database.js
+++ server/database/database.js
@@ -11,9 +11,9 @@ var DATABASE = {
     {
         this.pool = await mysql.createPool({
             host: process.env.DB_HOST || "localhost",
-            user: process.env.DB_USER || "root",
+            user: process.env.DB_USER || "Jabbon",
             password: process.env.DB_PASSWORD || "Cacahuete200$",
-            database: process.env.DB_DATABASE || "mydb"
+            database: process.env.DB_DATABASE || "JabbonDB"
         });
     },
     
